7e57407b2121745dd5268304226d065a993ea834
fxia22 <xf1280@gmail.com>
2017-08-02 18:17:37 -0700

    update

--- show_3d.py
+++ show_3d.py
@@ -2,16 +2,18 @@ import numpy as np
 import ctypes as ct
 import cv2
 import sys
+import argparse
+from datasets import ViewDataSet3D
+showsz = 256
 mousex,mousey=0.5,0.5
 changed=True
-
 
 def onmouse(*args):
     global mousex,mousey,changed
     y=args[1]
     x=args[2]
     mousex=x/float(showsz)
-    mousey=y/float(showsz)
+    mousey=y/float(showsz * 2)
     changed=True
 
 cv2.namedWindow('show3d')
@@ -20,12 +22,39 @@ cv2.setMouseCallback('show3d',onmouse)
 
 dll=np.ctypeslib.load_library('render','.')
 
-def showpoints():
+def showpoints(img, depth):
     global mousex,mousey,changed
+    show=np.zeros((showsz,showsz * 2,3),dtype='uint8')
     
+    
+    show = img
+    
+    def render():
+        pass
+    
+    while True:
+        cv2.imshow('show3d',show)
+        cmd=cv2.waitKey(10)%256
+    
+        if cmd==ord('q'):
+            break
     
 
 if __name__=='__main__':
-    np.random.seed(100)
-    showpoints()
-
+    
+    idx = 0
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--debug'  , action='store_true', help='debug mode')
+    parser.add_argument('--dataroot'  , required = True, help='dataset path')
+    opt = parser.parse_args()
+    d = ViewDataSet3D(root=opt.dataroot, transform = np.array, mist_transform = np.array, seqlen = 2)
+    
+    source = d[idx][0][0]
+    source_depth = d[idx][2][0]
+    
+    print(source.shape, source_depth.shape)
+    
+    showpoints(source, source_depth)
+    
+    
+    
