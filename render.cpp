d41105c07b9108b610a64a2dcb36c2e192fba612
fxia22 <xf1280@gmail.com>
2017-08-03 17:08:50 -0700

    add render

--- render.cpp
+++ render.cpp
@@ -4,3 +4,71 @@
 #include <math.h>
 using namespace std;
 
+extern "C"{
+    
+void render(int h,int w,unsigned char * img, float * depth,float * pose, unsigned char * render){
+    int ih, iw, i, ic;
+    
+    float * points3d = (float*) malloc(sizeof(float) * h * w * 4);
+    
+    for (i = 0; i < 5; i++) {
+        printf("%f ", pose[i]);
+    }
+    printf("\n");
+    for (ih = 0; ih < h; ih ++ ) {
+        for (iw = 0; iw < w; iw ++ ) {
+            for (ic = 0; ic < 3; ic ++) {
+                render[(ih * w + iw) * 3 +ic] = img[(ih * w + iw) * 3 + ic];
+            }
+        }
+    }
+    
+    for (ih = 0; ih < h; ih ++ ) {
+        for (iw = 0; iw < w; iw ++ ) {
+            float depth_point = depth[ih * w + iw];
+            float phi = ((float)(ih) + 0.5) / float(h) * M_PI;
+            float theta = ((float)(iw) + 0.5) / float(w) * M_PI;
+            points3d[(ih * w + iw) * 4 + 0] = depth_point * sin(phi) * cos(theta);
+            points3d[(ih * w + iw) * 4 + 1] = depth_point * sin(phi) * sin(theta);
+            points3d[(ih * w + iw) * 4 + 2] = depth_point * cos(phi);
+            points3d[(ih * w + iw) * 4 + 3] = 1;
+        }
+    }
+    
+    float alpha, beta, gamma, x, y, z;
+    x = pose[0];
+    y = pose[1];
+    z = pose[2];
+    alpha = pose[4];
+    beta = pose[3];
+    gamma = 0;
+    
+    float transformation_matrix[16];
+    
+    transformation_matrix[0] = cos(alpha) * cos(beta);
+    transformation_matrix[1] = cos(alpha) * sin(beta) * sin(gamma) - sin(alpha) * cos(gamma);
+    transformation_matrix[2] = cos(alpha) * sin(beta) * cos(gamma) + sin(alpha) * sin(gamma);
+    transformation_matrix[3] = x;
+    
+    transformation_matrix[4] = sin(alpha) * cos(beta);
+    transformation_matrix[5] = sin(alpha) * sin(beta) * sin(gamma) + cos(alpha) * cos(gamma);
+    transformation_matrix[6] = sin(alpha) * sin(beta) * cos(gamma) - cos(alpha) * sin(gamma);
+    transformation_matrix[7] = y;
+    transformation_matrix[8] = -sin(beta);
+    transformation_matrix[9] = cos(beta) * sin(gamma);
+    transformation_matrix[10] = cos(beta) * cos(gamma);
+    transformation_matrix[11] = z;
+    transformation_matrix[12] = 0;
+    transformation_matrix[13] = 0;
+    transformation_matrix[14] = 0;
+    transformation_matrix[15] = 1;
+    
+    
+    
+    
+    free(points3d);
+    
+}
+    
+    
+}//extern "C"
--- show_3d.py
+++ show_3d.py
@@ -7,14 +7,33 @@ from datasets import ViewDataSet3D
 showsz = 256
 mousex,mousey=0.5,0.5
 changed=True
+pitch,yaw,x,y,z = 0,0,0,0,0
+org_pitch, org_yaw, org_x, org_y, org_z = 0,0,0,0,0
+mousedown = False
+clickstart = (0,0)
 
 def onmouse(*args):
     global mousex,mousey,changed
-    y=args[1]
-    x=args[2]
-    mousex=x/float(showsz)
-    mousey=y/float(showsz * 2)
-    changed=True
+    global pitch,yaw,x,y,z
+    global org_pitch, org_yaw, org_x, org_y, org_z
+    global clickstart
+    
+    if args[0] == cv2.EVENT_LBUTTONDOWN:
+        org_pitch, org_yaw, org_x, org_y, org_z =\
+        pitch,yaw,x,y,z
+        clickstart = (mousex, mousey)
+
+        
+    if (args[3] & cv2.EVENT_FLAG_LBUTTON):
+        pitch = org_pitch + (mousex - clickstart[0])
+        yaw = org_yaw + (mousey - clickstart[1])
+        changed=True
+        
+    my=args[1]
+    mx=args[2]
+    mousex=mx/float(showsz)
+    mousey=my/float(showsz * 2)
+    
 
 cv2.namedWindow('show3d')
 cv2.moveWindow('show3d',0,0)
@@ -24,20 +43,46 @@ dll=np.ctypeslib.load_library('render','.')
 
 def showpoints(img, depth):
     global mousex,mousey,changed
+    global pitch,yaw,x,y,z
     show=np.zeros((showsz,showsz * 2,3),dtype='uint8')
     
-    
-    show = img
-    
-    def render():
-        pass
+    def render(img, depth, pose):
+        dll.render(ct.c_int(img.shape[0]),
+                   ct.c_int(img.shape[1]),
+                   img.ctypes.data_as(ct.c_void_p),
+                   depth.ctypes.data_as(ct.c_void_p),
+                   pose.ctypes.data_as(ct.c_void_p),
+                   show.ctypes.data_as(ct.c_void_p)
+                  )
+        
     
     while True:
+        
+        if changed:
+            render(img, depth, np.array([x,y,z,pitch,yaw]).astype(np.float32))
+            changed = False
+                
+        cv2.putText(show,'pitch %.2f yaw %.2f x %.2f y %.2f z %.2f'%(pitch, yaw, x, y, z),(15,showsz-15),0,0.5,cv2.cv.CV_RGB(255,0,0))
+        
         cv2.imshow('show3d',show)
         cmd=cv2.waitKey(10)%256
     
         if cmd==ord('q'):
             break
+            
+        elif cmd == ord('w'):
+            y += 0.01
+            changed = True
+        elif cmd == ord('s'):
+            y -= 0.01
+            changed = True
+        elif cmd == ord('a'):
+            x -= 0.01
+            changed = True
+        elif cmd == ord('d'):
+            x += 0.01    
+            changed = True
+            
     
 
 if __name__=='__main__':
