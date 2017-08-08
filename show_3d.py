8cf98ea91e47507ba87c88e50cc35904710950ac
fxia22 <xf1280@gmail.com>
2017-08-08 15:25:08 -0700

    misalignment debug

--- show_3d.py
+++ show_3d.py
@@ -30,7 +30,7 @@ def onmouse(*args):
     global org_pitch, org_yaw, org_x, org_y, org_z
     global org_roll, roll
     global clickstart
-    
+
     if args[0] == cv2.EVENT_LBUTTONDOWN:
         org_pitch, org_yaw, org_x, org_y, org_z =\
         pitch,yaw,x,y,z
@@ -39,21 +39,21 @@ def onmouse(*args):
     if args[0] == cv2.EVENT_RBUTTONDOWN:
         org_roll = roll
         clickstart = (mousex, mousey)
-        
+
     if (args[3] & cv2.EVENT_FLAG_LBUTTON):
         pitch = org_pitch + (mousex - clickstart[0])/10
         yaw = org_yaw + (mousey - clickstart[1])
         changed=True
-    
+
     if (args[3] & cv2.EVENT_FLAG_RBUTTON):
         roll = org_roll + (mousex - clickstart[0])/50
         changed=True
-        
+
     my=args[1]
     mx=args[2]
     mousex=mx/float(showsz)
     mousey=my/float(showsz * 2)
-    
+
 
 
 def showpoints(img, depth, pose, model):
@@ -67,7 +67,7 @@ def showpoints(img, depth, pose, model):
     cv2.namedWindow('show3d')
     cv2.moveWindow('show3d',0,0)
     cv2.setMouseCallback('show3d',onmouse)
-    
+
     imgv = Variable(torch.zeros(1,3, 256, 512)).cuda()
     maskv = Variable(torch.zeros(1,1, 256, 512)).cuda()
 
@@ -87,46 +87,46 @@ def showpoints(img, depth, pose, model):
             source = tf(show)
             source_depth = tf(np.expand_dims(target_depth, 2))
             #print(source.size(), source_depth.size())
-        
+
             imgv.data.copy_(source)
             maskv.data.copy_(source_depth)
-            
+
             recon = model(imgv, maskv)
             #print(recon.size())
             show2 = recon.data.cpu().numpy()[0].transpose(1,2,0)
             show[:] = (show2[:] * 255).astype(np.uint8)
-            
+
         t1 = time.time()
         t = t1-t0
         fps = 1/t
-      
+
         cv2.waitKey(5)%256
-            
+
     while True:
-        
+
         if changed:
             render(img, depth, np.array([x,y,z,pitch,yaw,roll]).astype(np.float32), model)
             changed = False
-                
-        
+
+
         if overlay:
             show_out = (show/2 + target/2).astype(np.uint8)
         elif show_depth:
             show_out = (target_depth * 10).astype(np.uint8)
         else:
             show_out = show
-        
+
         cv2.putText(show,'pitch %.3f yaw %.2f roll %.3f x %.2f y %.2f z %.2f'%(pitch, yaw, roll, x, y, z),(15,showsz-15),0,0.5,cv2.cv.CV_RGB(255,255,255))
         cv2.putText(show,'fps %.1f'%(fps),(15,15),0,0.5,cv2.cv.CV_RGB(255,255,255))
-        
+
         show_rgb = cv2.cvtColor(show_out, cv2.COLOR_BGR2RGB)
         cv2.imshow('show3d',show_rgb)
-        
+
         cmd=cv2.waitKey(5)%256
-    
+
         if cmd==ord('q'):
             break
-            
+
         elif cmd == ord('w'):
             x -= 0.05
             changed = True
@@ -137,7 +137,7 @@ def showpoints(img, depth, pose, model):
             y += 0.05
             changed = True
         elif cmd == ord('d'):
-            y -= 0.05    
+            y -= 0.05
             changed = True
         elif cmd == ord('r'):
             pitch,yaw,x,y,z = 0,0,0,0,0
@@ -156,7 +156,7 @@ def showpoints(img, depth, pose, model):
         elif cmd == ord('f'):
             show_depth = not show_depth
 
-    
+
 def show_target(target_img):
     cv2.namedWindow('target')
     cv2.moveWindow('target',0,256 + 50)
@@ -165,7 +165,7 @@ def show_target(target_img):
     cv2.imshow('target', show_rgb)
 
 if __name__=='__main__':
-    
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--debug'  , action='store_true', help='debug mode')
     parser.add_argument('--dataroot'  , required = True, help='dataset path')
