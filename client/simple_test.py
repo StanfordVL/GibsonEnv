dc09cdca75a332e588a907aa2f1b4093dbce8ea2
Zhiyang He <zhiyanghe96@gmail.com>
2017-08-16 15:30:25 -0700

    Added client folder

--- client/simple_test.py
+++ client/simple_test.py
@@ -1,20 +1,28 @@
 import go_vncdriver
 import time
+import yaml
+import os
 
 from PIL import Image
 
 from gym.envs.classic_control import rendering
+
+remote_file = os.path.join(os.path.dirname(__file__), '/remote.yml')
+with open('./remote.yml') as f:
+    spec = yaml.load(f)
+
+print('{}:{}'.format(spec['capri']['addr'], spec['capri']['port']))
 
 # res = go_vncdriver.example(1, 2)
 # res = go_vncdriver.connect(["hi", "tester"])
 # import ipdb;ipdb.set_trace()
 
 # go_vncdriver.setup()
-h = go_vncdriver.VNCSession({'address': 
-			'capri19.stanford.edu:5901',
+h = go_vncdriver.VNCSession({'address': '{}:{}'.format(spec['capri']['addr'], spec['capri']['port']),
 			'password': 'qwertyui',
 			'name': 'conn',
 		}, None)
+
 print(h.remotes)
 
 viewer = rendering.SimpleImageViewer()
