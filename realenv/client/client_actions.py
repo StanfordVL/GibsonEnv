1d38c74c5b11ef81b8ab9c5417e39bcb6c1d5a1a
Zhiyang He <zhiyanghe96@gmail.com>
2017-08-17 11:29:10 -0700

    Client actions enabled 'j' & 'l'

--- realenv/client/client_actions.py
+++ realenv/client/client_actions.py
@@ -1,2 +1,16 @@
-client_actions = ['w', 'a', 's', 'd', 'z', 'x']
-client_newloc  = ['n']
+client_actions = [
+	'w', 	# Move forward	
+	'a', 	# Move left
+	's', 	# Move backward
+	'd', 	# Move right
+	'z', 	# Move downward
+	'x',	# Move upward
+	'j',	# Turn left
+	'l' 	# Turn right
+]
+
+
+
+client_newloc  = [
+	'n'		# Bring the view to a new starting location
+]
