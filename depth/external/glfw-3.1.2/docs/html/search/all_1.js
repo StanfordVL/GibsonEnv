41db9aee35f104a389fcc4a70c8ab335a09d7695
fxia22 <xf1280@gmail.com>
2017-09-13 22:25:38 -0700

    remove libs

--- depth/external/glfw-3.1.2/docs/html/search/all_1.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['compat_2edox',['compat.dox',['../compat_8dox.html',1,'']]],
-  ['compiling_20glfw',['Compiling GLFW',['../compile.html',1,'']]],
-  ['compile_2edox',['compile.dox',['../compile_8dox.html',1,'']]],
-  ['context_20handling',['Context handling',['../group__context.html',1,'(Global Namespace)'],['../context.html',1,'(Global Namespace)']]],
-  ['context_2edox',['context.dox',['../context_8dox.html',1,'']]]
-];
--- depth/external/glfw-3.1.2/docs/html/search/pages_8.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['window_20guide',['Window guide',['../window.html',1,'']]]
-];
