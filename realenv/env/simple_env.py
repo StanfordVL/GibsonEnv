1e24da1d19d6f75ffc48322053cedba8a6bf6f56
hzyjerry <zhiyanghe96@gmail.com>
2017-10-12 12:13:14 -0700

    Simple husky reward senario

--- realenv/env/simple_env.py
+++ realenv/env/simple_env.py
@@ -13,6 +13,7 @@ import random
 import progressbar
 from realtime_plot import RewardDisplayer
 
+
 class SimpleEnv(gym.Env):
   metadata = {'render.modes': ['human']}
 
@@ -29,17 +30,27 @@ class SimpleEnv(gym.Env):
     #self.p_render  = subprocess.Popen()
 
     try:
-      self.r_physics = self._setupPhysics()
       self.r_visuals = self._setupVisuals()
-
-      pose_init = self.r_visuals.renderOffScreenInitialPose()
-      self.r_physics.initialize(pose_init)
+      pose_init = self.r_visuals.renderOffScreenInitialPose()    
     except Exception as e:
       print(e)
       self._end()
+    
+    self.r_physics = self._setupPhysics()
+    self.r_physics.initialize(pose_init)
     if self.debug_mode:
       self.r_visuals.renderToScreenSetup()
       self.r_displayer = RewardDisplayer()
+    self._setupRewardFunc()
+    self.state_old = None
+
+  def _setupRewardFunc(self):
+    def _getReward(state_old, state_new):
+      if not state_old:
+        return 0
+      else:
+        return 5 * (state_old['distance_to_target'] - state_new['distance_to_target'])
+    self.reward_func = _getReward
     
   def _setupVisuals(self):
     d = ViewDataSet3D(root=self.datapath, transform = np.array, mist_transform = np.array, seqlen = 2, off_3d = False, train = False)
@@ -72,12 +83,11 @@ class SimpleEnv(gym.Env):
         poses.append(pose)
         sources.append(target)
         source_depths.append(target_depth)
-    print('target', poses, poses[0])
+    #print('target', poses, poses[0])
     #print('no.1 pose', poses, poses[1])
     # print(source_depth)
     print(sources[0].shape, source_depths[0].shape)
     context_mist = zmq.Context()
-    print("Connecting to hello world server...")
     socket_mist = context_mist.socket(zmq.REQ)
     socket_mist.connect("tcp://localhost:5555")
     
@@ -99,12 +109,16 @@ class SimpleEnv(gym.Env):
   def _step(self, action):
     #renderer.renderToScreen(sources, source_depths, poses, model, target, target_depth, rts)
     if not self.debug_mode:
-      pose = self.r_physics.renderOffScreen(action)
-      reward = random.randrange(-8, 20)
+      pose, state = self.r_physics.renderOffScreen(action)
+      #reward = random.randrange(-8, 20)
+      reward = self.reward_func(self.state_old, state)
+      self.state_old = state
       visuals = self.r_visuals.renderOffScreen(pose)
     else:
-      pose = self.r_physics.renderToScreen(action)
-      reward = random.randrange(-8, 20)
+      pose, state = self.r_physics.renderToScreen(action)
+      #reward = random.randrange(-8, 20)
+      reward = self.reward_func(self.state_old, state)
+      self.state_old = state
       self.r_displayer.add_reward(reward)
       visuals = self.r_visuals.renderToScreen(pose)
     return visuals, reward 
