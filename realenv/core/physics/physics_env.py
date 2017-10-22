8cb6f5b974bc3a572798b4fdf037c9fd1d374ca6
hzyjerry <zhiyanghe96@gmail.com>
2017-10-22 02:45:21 -0700

    Humanoid model in indoor scene with friction

--- realenv/core/physics/physics_env.py
+++ realenv/core/physics/physics_env.py
@@ -14,24 +14,29 @@ import settings
 from transforms3d import euler, quaternions
 from realenv.core.physics.physics_object import PhysicsObject
 from realenv.core.render.profiler import Profiler
+from realenv.core.physics.env_bases import MJCFBaseBulletEnv
+from realenv.core.physics.robot_locomotors import Humanoid
+from .scene_building import SinglePlayerBuildingScene
+import gym
 
 
-class PhysicsEnv(object):
+class PhysicsEnv(MJCFBaseBulletEnv):
     metadata = {
         'render.modes': ['human_eye', 'rgb_array', 'human_play'],
         'video.frames_per_second' : 20
     }
 
-    def __init__(self, obj_path, render_mode, pose, fps = 12):
+    def __init__(self, obj_path, render_mode, pose, update_freq = 12):
+        MJCFBaseBulletEnv.__init__(self, robot, render=False)
         self.debug_sliders = {}
         self.r_mode     = render_mode
         self.init_pose  = pose
 
-        ## The visual render runs at fps, but in order to avoid wall crossing
+        ## The visual render runs at update_freq, but in order to avoid wall crossing
         ## and other issues in physics renderer, we need to keep physics
         ## engine running at a minimum 100 frames per sec
-        self.step_count  = int(math.ceil(100.0 / fps))
-        self.time_step   = 1.0 / (fps * self.step_count)
+        self.action_repeat  = int(math.ceil(100.0 / update_freq))
+        self.time_step      = 1.0 / (update_freq * self.action_repeat)
         
         self._setup_context(obj_path)
         self._set_gravity()
@@ -41,6 +46,7 @@ class PhysicsEnv(object):
 
 
     def _reset(self, pose=None):
+        r = MJCFBaseBulletEnv._reset(self)
         file_dir = os.path.dirname(os.path.abspath(__file__))
         self.objectUid = p.loadURDF(os.path.join(file_dir, "models/quadrotor.urdf"), globalScaling = 0.8)
         #self.objectUid = p.loadURDF(os.path.join(file_dir, "models/husky.urdf"), globalScaling = 0.8)
@@ -53,7 +59,7 @@ class PhysicsEnv(object):
             pos, quat_xyzw = pose[0], pose[1]
         else:
             pos, quat_xyzw = pose_init[0], pose_init[1]
-        self.hero = PhysicsObject(self.objectUid, p, pos, quat_xyzw, v_t, v_r)
+        self.robot = PhysicsObject(self.objectUid, p, pos, quat_xyzw, v_t, v_r)
         print("Generated cart", self.objectUid)
 
 
@@ -84,23 +90,23 @@ class PhysicsEnv(object):
         p.setRealTimeSimulation(0)
 
 
-    def _render(self, action, restart=False):
+    def _step(self, action, restart=False):
         """Execute one frame"""
         if self.r_mode == "human_eye" or self.r_mode == "rgb_array":
-            self.hero.parseActionAndUpdate(action)
+            self.robot.parseActionAndUpdate(action)
         elif self.r_mode == "human_play":
-            self.hero.getUpdateFromKeyboard(restart=restart)
+            self.robot.getUpdateFromKeyboard(restart=restart)
         else:
             raise Exception 
 
-        print("step count", self.step_count)
+        print("action repeat", self.action_repeat)
         with Profiler("Physics internal"):
-            for s in range(self.step_count):
+            for s in range(self.action_repeat):
                 p.stepSimulation()
         
         self._update_debug_panels()
 
-        pos_xyz, quat_wxyz = self.hero.getViewPosAndOrientation()
+        pos_xyz, quat_wxyz = self.robot.getViewPosAndOrientation()
         state = {
             'distance_to_target': np.sum(np.square(pos_xyz - self.target_pos))
         }
@@ -129,7 +135,7 @@ class PhysicsEnv(object):
             cameraYaw  = p.readUserDebugParameter(self.debug_sliders['yaw'])
             cameraPitch = p.readUserDebugParameter(self.debug_sliders['pitch'])
             
-            pos_xyz, quat_wxyz = self.hero.getViewPosAndOrientation()
+            pos_xyz, quat_wxyz = self.robot.getViewPosAndOrientation()
             p.getCameraImage(256, 256, viewMatrix = self.viewMatrix, projectionMatrix = self.projMatrix)
             p.resetDebugVisualizerCamera(cameraDist, cameraYaw, cameraPitch, pos_xyz)
 
@@ -143,3 +149,121 @@ class PhysicsEnv(object):
             p.stepSimulation()
             pObject.parseActionAndUpdate()
         pObject.clearUpDelta()
+
+
+
+class PhysicsExtendedEnv(MJCFBaseBulletEnv):
+    def __init__(self, robot, render=False):
+        print("PhysicsExtendedEnv::__init__")
+        MJCFBaseBulletEnv.__init__(self, robot, render)
+        
+        self.camera_x = 0
+        self.walk_target_x = 1e3  # kilometer away
+        self.walk_target_y = 0
+
+    def create_single_player_scene(self):
+        self.building_scene = SinglePlayerBuildingScene(gravity=9.8, timestep=0.0165/4, frame_skip=4)
+        return self.building_scene
+
+    def _reset(self):
+        
+        r = MJCFBaseBulletEnv._reset(self)
+        p.configureDebugVisualizer(p.COV_ENABLE_RENDERING,0)
+
+        print("reset", self.building_scene.building_obj)
+        self.parts, self.jdict, self.ordered_joints, self.robot_body = self.robot.addToScene(
+            self.building_scene.building_obj)
+        #print(self.parts)
+        #self.ground_ids = set([(self.parts[f].bodies[self.parts[f].bodyIndex], self.parts[f].bodyPartIndex) for f in self.foot_ground_object_names])
+        self.ground_ids = set([(self.building_scene.building_obj, 0)])
+        p.configureDebugVisualizer(p.COV_ENABLE_RENDERING,1)
+        return r
+
+    def move_robot(self, init_x, init_y, init_z):
+        "Used by multiplayer building to move sideways, to another running lane."
+        self.cpp_robot.query_position()
+        pose = self.cpp_robot.root_part.pose()
+        pose.move_xyz(init_x, init_y, init_z)  # Works because robot loads around (0,0,0), and some robots have z != 0 that is left intact
+        self.cpp_robot.set_pose(pose)
+
+    electricity_cost     = -2.0 # cost for using motors -- this parameter should be carefully tuned against reward for making progress, other values less improtant
+    stall_torque_cost   = -0.1  # cost for running electric current through a motor even at zero rotational speed, small
+    foot_collision_cost  = -1.0 # touches another leg, or other objects, that cost makes robot avoid smashing feet into itself
+    foot_ground_object_names = set(["buildingFloor"])  # to distinguish ground and other objects
+    joints_at_limit_cost = -0.1 # discourage stuck joints
+
+    def _step(self, a):
+        if not self.scene.multiplayer:  # if multiplayer, action first applied to all robots, then global step() called, then _step() for all robots with the same actions
+            self.robot.apply_action(a)
+            self.scene.global_step()
+
+        state = self.robot.calc_state()  # also calculates self.joints_at_limit
+
+        alive = float(self.robot.alive_bonus(state[0]+self.robot.initial_z, self.robot.body_rpy[1]))   # state[0] is body height above ground, body_rpy[1] is pitch
+        done = alive < 0
+        if not np.isfinite(state).all():
+            print("~INF~", state)
+            done = True
+
+        potential_old = self.potential
+        self.potential = self.robot.calc_potential()
+        progress = float(self.potential - potential_old)
+
+        feet_collision_cost = 0.0
+        for i,f in enumerate(self.robot.feet): # TODO: Maybe calculating feet contacts could be done within the robot code
+            print(f.contact_list())
+            contact_ids = set((x[2], x[4]) for x in f.contact_list())
+            #print("CONTACT OF '%d' WITH %d" % (contact_ids, ",".join(contact_names)) )
+            if (self.ground_ids & contact_ids):
+                            #see Issue 63: https://github.com/openai/roboschool/issues/63
+                #feet_collision_cost += self.foot_collision_cost
+                self.robot.feet_contact[i] = 1.0
+            else:
+                self.robot.feet_contact[i] = 0.0
+        print(self.robot.feet_contact)
+
+        electricity_cost  = self.electricity_cost  * float(np.abs(a*self.robot.joint_speeds).mean())  # let's assume we have DC motor with controller, and reverse current braking
+        electricity_cost += self.stall_torque_cost * float(np.square(a).mean())
+
+        joints_at_limit_cost = float(self.joints_at_limit_cost * self.robot.joints_at_limit)
+        debugmode=0
+        if(debugmode):
+            print("alive=")
+            print(alive)
+            print("progress")
+            print(progress)
+            print("electricity_cost")
+            print(electricity_cost)
+            print("joints_at_limit_cost")
+            print(joints_at_limit_cost)
+            print("feet_collision_cost")
+            print(feet_collision_cost)
+
+        self.rewards = [
+            alive,
+            progress,
+            electricity_cost,
+            joints_at_limit_cost,
+            feet_collision_cost
+            ]
+        if (debugmode):
+            print("rewards=")
+            print(self.rewards)
+            print("sum rewards")
+            print(sum(self.rewards))
+        self.HUD(state, a, done)
+        self.reward += sum(self.rewards)
+
+        return state, sum(self.rewards), bool(done), {}
+
+    def camera_adjust(self):
+        x, y, z = self.body_xyz
+        self.camera_x = 0.98*self.camera_x + (1-0.98)*x
+        self.camera.move_and_look_at(self.camera_x, y-2.0, 1.4, x, y, 1.0)
+
+
+
+class HumanoidWalkingEnv(PhysicsExtendedEnv):
+    def __init__(self):
+        self.robot = Humanoid()
+        PhysicsExtendedEnv.__init__(self, self.robot)
---
+++ realenv/core/physics/scene_building.py
@@ -0,0 +1,68 @@
+import os,  inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+os.sys.path.insert(0,parentdir)
+import pybullet_data
+
+
+from realenv.core.physics.scene_abstract import Scene
+import pybullet as p
+
+
+class BuildingScene(Scene):
+    def episode_restart(self):
+        Scene.episode_restart(self)   # contains cpp_world.clean_everything()
+        # stadium_pose = cpp_household.Pose()
+        # if self.zero_at_running_strip_start_line:
+        #    stadium_pose.set_xyz(27, 21, 0)  # see RUN_STARTLINE, RUN_RAD constants
+        #filename = "/home/jerry/Desktop/realenv/realenv/data/dataset/11HB6XZSh1Q/modeldata/building.urdf"
+        filename = "/home/jerry/Desktop/realenv/realenv/data/dataset/11HB6XZSh1Q/modeldata/out_z_up.obj"
+        collisionId = p.createCollisionShape(p.GEOM_MESH, fileName=filename, meshScale=[1, 1, 1], flags=p.GEOM_FORCE_CONCAVE_TRIMESH)
+        visualId = p.createVisualShape(p.GEOM_MESH, fileName=filename, meshScale=[1, 1, 1], rgbaColor = [1, 0.2, 0.2, 0.3], specularColor=[0.4, 4.0])
+        boundaryUid = p.createMultiBody(baseCollisionShapeIndex = collisionId, baseVisualShapeIndex = visualId)
+        #p.changeVisualShape(boundaryUid, -1, rgbaColor=[1, 0.2, 0.2, 0.3], specularColor=[1, 1, 1])
+        #self.building_obj = [collisionId]
+        #planeName = os.path.join(pybullet_data.getDataPath(),"mjcf/ground_plane.xml")
+        #self.ground_plane_mjcf = p.loadMJCF(planeName)
+        #print("built plane", type(self.ground_plane_mjcf))
+        p.changeDynamics(boundaryUid, -1, lateralFriction=0.8, spinningFriction=0.1, rollingFriction=0.1)
+        self.building_obj = (boundaryUid, )
+        #self.building_obj = (int(p.loadURDF(filename)), )
+        print("built building scene", self.building_obj)
+        for i in self.building_obj:
+            #collisionId = p.createCollisionShape(p.GEOM_MESH, fileName=filename, meshScale=[1, 1, 1], flags=p.GEOM_FORCE_CONCAVE_TRIMESH)
+            p.changeVisualShape(i,-1,rgbaColor=[1,0.2,0.2,0.3])
+
+
+class SinglePlayerBuildingScene(BuildingScene):
+    multiplayer = False
+
+class StadiumScene(Scene):
+    zero_at_running_strip_start_line = True   # if False, center of coordinates (0,0,0) will be at the middle of the stadium
+    stadium_halflen   = 105*0.25    # FOOBALL_FIELD_HALFLEN
+    stadium_halfwidth = 50*0.25  # FOOBALL_FIELD_HALFWID
+
+    def episode_restart(self):
+        Scene.episode_restart(self)   # contains cpp_world.clean_everything()
+        # stadium_pose = cpp_household.Pose()
+        # if self.zero_at_running_strip_start_line:
+        #    stadium_pose.set_xyz(27, 21, 0)  # see RUN_STARTLINE, RUN_RAD constants
+        filename = os.path.join(pybullet_data.getDataPath(),"stadium_no_collision.sdf")
+        self.stadium = p.loadSDF(filename)
+        planeName = os.path.join(pybullet_data.getDataPath(),"mjcf/ground_plane.xml")
+        
+        self.ground_plane_mjcf = p.loadMJCF(planeName)
+        for i in self.ground_plane_mjcf:
+            p.changeVisualShape(i,-1,rgbaColor=[0,0,0,0])
+
+class SinglePlayerStadiumScene(StadiumScene):
+    "This scene created by environment, to work in a way as if there was no concept of scene visible to user."
+    multiplayer = False
+
+class MultiplayerStadiumScene(StadiumScene):
+    multiplayer = True
+    players_count = 3
+    def actor_introduce(self, robot):
+        StadiumScene.actor_introduce(self, robot)
+        i = robot.player_n - 1  # 0 1 2 => -1 0 +1
+        robot.move_robot(0, i, 0)
