cac72e7daeb4389b08f23b48798d442bd051bc3e
hzyjerry <zhiyanghe96@gmail.com>
2017-11-09 03:43:58 -0800

    Play ant env

---
+++ examples/demo/play_ant_camera.py
@@ -0,0 +1,9 @@
+from realenv.envs.ant_env import AntNavigateEnv
+from realenv.utils.play import play
+
+timestep = 1.0/(4 * 22)
+frame_skip = 4
+
+if __name__ == '__main__':
+    env = AntNavigateEnv(human=True, timestep=timestep, frame_skip=frame_skip, mode="RGBD", is_discrete = True, resolution="MID")
+    play(env, zoom=4, fps=int( 1.0/(timestep * frame_skip)))
