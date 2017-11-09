d128f9dad35616f37419bf2c6d303f2beb847d4d
fxia22 <xf1280@gmail.com>
2017-11-09 01:59:54 -0800

    add histogram matching

--- realenv/core/render/show_3d2.py
+++ realenv/core/render/show_3d2.py
@@ -34,6 +34,56 @@ LINUX_OFFSET = {
     "y_delta": 100
 }
 
+def hist_match(source, template):
+    """
+    Adjust the pixel values of a grayscale image such that its histogram
+    matches that of a target image
+
+    Arguments:
+    -----------
+        source: np.ndarray
+            Image to transform; the histogram is computed over the flattened
+            array
+        template: np.ndarray
+            Template image; can have different dimensions to source
+    Returns:
+    -----------
+        matched: np.ndarray
+            The transformed output image
+    """
+
+    oldshape = source.shape
+    source = source.ravel()
+    template = template.ravel()
+
+    # get the set of unique pixel values and their corresponding indices and
+    # counts
+    s_values, bin_idx, s_counts = np.unique(source, return_inverse=True,
+                                            return_counts=True)
+    t_values, t_counts = np.unique(template, return_counts=True)
+
+    # take the cumsum of the counts and normalize by the number of pixels to
+    # get the empirical cumulative distribution functions for the source and
+    # template images (maps pixel value --> quantile)
+    s_quantiles = np.cumsum(s_counts).astype(np.float64)
+    s_quantiles /= s_quantiles[-1]
+    t_quantiles = np.cumsum(t_counts).astype(np.float64)
+    t_quantiles /= t_quantiles[-1]
+
+    # interpolate linearly to find the pixel values in the template image
+    # that correspond most closely to the quantiles in the source image
+    interp_t_values = np.interp(s_quantiles, t_quantiles, t_values)
+
+    return interp_t_values[bin_idx].reshape(oldshape)
+
+
+def hist_match3(source, template):
+    s0 = hist_match(source[:,:,0], template[:,:,0])
+    s1 = hist_match(source[:,:,1], template[:,:,1])
+    s2 = hist_match(source[:,:,2], template[:,:,2])
+    return np.stack([s0,s1,s2], axis = 2)
+
+
 
 class InImg(object):
     def __init__(self):
@@ -313,6 +363,15 @@ class PCRenderer:
                 show[:] = (show2[:] * 255).astype(np.uint8)
 
         self.target_depth = opengl_arr ## target depth
+
+        #Histogram matching happens here
+        template = (show_unfilled/255.0).astype(np.float32)
+        source = (show/255.0).astype(np.float32)
+        source_matched = hist_match3(source, template)
+        show[:] = (source_matched[:] * 255).astype(np.uint8)
+
+
+
 
 
 
