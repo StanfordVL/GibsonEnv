79c6415c59123988f2a30e521cb24282c113bf02
fxia22 <xf1280@gmail.com>
2017-11-13 13:44:35 -0800

    tune torque

--- realenv/core/physics/robot_locomotors.py
+++ realenv/core/physics/robot_locomotors.py
@@ -346,7 +346,7 @@ class Husky(WalkerBase):
 
         if self.is_discrete:
             self.action_space = gym.spaces.Discrete(5)
-            self.torque = 0.1
+            self.torque = 0.2
             self.action_list = [[self.torque/10, self.torque/10, self.torque/10, self.torque/10],
                                 [-self.torque * 2, -self.torque * 2, -self.torque * 2, -self.torque * 2],
                                 [self.torque, -self.torque, self.torque, -self.torque],
