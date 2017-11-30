3574b75b6f15033f55f02846f0977195e60b8447
fxia22 <xf1280@gmail.com>
2017-11-29 21:24:02 -0600

    remove obsolete files

--- realenv/core/tests/test_engine.py
+++
@@ -1,8 +0,0 @@
-from realenv.core.engine import Engine
-import time
-
-test_engine = Engine(model_id="11HB6XZSh1Q", human=True, debug=False)
-test_engine.setup_all()
-
-#for i in range(3):
-#    time.sleep(1)
