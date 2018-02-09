095a66ac38d2b516aded8f661937008109f9acd9
fxia22 <xf1280@gmail.com>
2018-02-08 23:18:46 -0800

    all demo scripts work fine

--- examples/demo/play_humanoid_camera.py
+++ examples/demo/play_humanoid_camera.py
@@ -1,15 +1,14 @@
 from gibson.envs.humanoid_env import HumanoidNavigateEnv
 from gibson.utils.play import play
-
-timestep = 1.0/200
-frame_skip = 1
-
+import os
+timestep = 1.0/(4 * 22)
+frame_skip = 4
+config_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'configs', 'humanoid_navigate.yaml')
+print(config_file)
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--resolution', type=str, default="LARGE")
     args = parser.parse_args()
 
-    #env = HuskyNavigateEnv(human=True, timestep=timestep, frame_skip=frame_skip, mode="SENSOR", is_discrete = True, resolution="MID")
-    env = HumanoidNavigateEnv(human=True, timestep=timestep, frame_skip=frame_skip, mode="RGBD", is_discrete = True, resolution=args.resolution)
+    env = HumanoidNavigateEnv(human=True, is_discrete = True, config = config_file)
     play(env, zoom=4, fps=int( 1.0/(timestep * frame_skip)))
--- examples/demo/play_humanoid_nonviz.py
+++ examples/demo/play_humanoid_nonviz.py
@@ -1,10 +1,14 @@
 from gibson.envs.humanoid_env import HumanoidNavigateEnv
 from gibson.utils.play import play
+import os
 
 timestep = 1.0/(4 * 22)
 frame_skip = 4
+config_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'configs', 'humanoid_navigate.yaml')
+print(config_file)
+
 
 if __name__ == '__main__':
     #env = HuskyNavigateEnv(human=True, timestep=timestep, frame_skip=frame_skip, mode="SENSOR", is_discrete = True, resolution="MID")
-    env = HumanoidNavigateEnv(human=True, timestep=timestep, frame_skip=frame_skip, mode="SENSOR", is_discrete = True, resolution="MID")
+    env = HumanoidNavigateEnv(human=True, is_discrete = True, config=config_file)
     play(env, zoom=4, fps=int( 1.0/(timestep * frame_skip)))
