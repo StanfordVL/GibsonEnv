c42b65563301b9dc97e90f6542f130ea603a633d
micheleantonazzi <m7242872428>
2021-05-06 12:47:08 +0200

    fixed upgrade cmake

--- build_scripts/before_all_linux.sh
+++ build_scripts/before_all_linux.sh
@@ -9,7 +9,7 @@ yum install openmpi* libmpi* -y
 export CC=/usr/lib64/openmpi/bin/mpicc
 
 # Install cmake requirements
-yum install -y freeglut* yum-utils glew* libXrandr* yum install libXinerama* install libXcursor* boost boost-devel minizip* asciidoc zeromq3*
+yum install -y freeglut* yum-utils glew* libXrandr* yum install libXinerama* install libXcursor* boost boost-devel minizip* asciidoc zeromq3* wget
 
 # Install zeromq
 wget https://github.com/zeromq/libzmq/releases/download/v4.2.5/zeromq-4.2.5.zip
@@ -29,6 +29,7 @@ yum install -y cuda
 export PATH=/usr/local/cuda-10.2/bin${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=/usr/local/cuda-10.2/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 nvcc --version
+
 
 wget https://cmake.org/files/v3.5/cmake-3.5.2.tar.gz
 tar -zxvf cmake-3.5.2.tar.gz
