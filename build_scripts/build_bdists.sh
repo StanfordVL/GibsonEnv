2eaa6a01614722f495bfc7e3d93cc3c21e3527ac
micheleantonazzi <micheleantonazzi@gmail.com>
2021-05-07 21:50:31 +0200

    fixed declaration of hastable i n bash

--- build_scripts/build_bdists.sh
+++ build_scripts/build_bdists.sh
@@ -4,6 +4,13 @@
 # Install pip requirements
 export CC=/usr/lib64/openmpi/bin/mpicc
 
+declare -A python_versions=(
+  ['3.6']='https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tar.xz'
+  ['3.7']='https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tar.xz'
+  ['3.8']='https://www.python.org/ftp/python/3.8.9/Python-3.8.9.tar.xz'
+  ['3.9']='https://www.python.org/ftp/python/3.9.4/Python-3.9.4.tar.xz'
+)
+
 for version in "${!python_versions[@]}"; do
   pip$version install wheel auditwheel twine
   pip$version install -r build_scripts/requirements.txt
--- build_scripts/setup_python_versions.sh
+++ build_scripts/setup_python_versions.sh
@@ -21,7 +21,7 @@ function install_python() {
 yum groupinstall -y "Development Tools"
 yum install -y gcc bzip2-devel sqlite-devel zlib-devel wget libffi-devel openssl-devel
 
-declare -A -g python_versions=(
+declare -A python_versions=(
   ['3.6']='https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tar.xz'
   ['3.7']='https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tar.xz'
   ['3.8']='https://www.python.org/ftp/python/3.8.9/Python-3.8.9.tar.xz'
