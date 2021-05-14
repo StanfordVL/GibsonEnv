0c25fd4b2700b1781280cf5f42ba6f8f9803ec79
micheleantonazzi <micheleantonazzi@gmail.com>
2021-05-14 17:30:16 +0200

    tried to use matrix strategy foo building manylinux2010

--- build_scripts/manylinux2010/build_bdists.sh
+++ build_scripts/manylinux2010/build_bdists.sh
@@ -4,19 +4,10 @@
 # Install pip requirements
 export CC=/usr/lib64/openmpi/bin/mpicc
 
-declare -A python_versions=(
-  ['3.6']='https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tar.xz'
-  ['3.7']='https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tar.xz'
-  ['3.8']='https://www.python.org/ftp/python/3.8.9/Python-3.8.9.tar.xz'
-  #['3.9']='https://www.python.org/ftp/python/3.9.4/Python-3.9.4.tar.xz'
-)
+pip3 install wheel auditwheel
+pip3 install -r build_scripts/manylinux2010/requirements.txt --no-cache-dir
+pip3 install -e .
+python3 setup.py bdist_wheel
+python3 -m auditwheel repair dist/gibson-*-cp*
 
-for version in "${!python_versions[@]}"; do
-  pip$version install wheel auditwheel
-  pip$version install -r build_scripts/manylinux2010/requirements.txt --no-cache-dir
-  pip$version install -e .
-  python$version setup.py bdist_wheel
-  python$version -m auditwheel repair dist/gibson-*-cp"${version//.}"*
-  rm -rf /tmp/pip*
-done
 
